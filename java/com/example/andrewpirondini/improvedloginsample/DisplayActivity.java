6169aa8e18685e38d4433fe6a63dcd89557e15d3
Bradpat28 <Bradpat28@Brads-MacBook-Pro.local>
2015-10-24 12:32:08 -0400

    OH files

--- java/com/example/andrewpirondini/improvedloginsample/DisplayActivity.java
+++ java/com/example/andrewpirondini/improvedloginsample/DisplayActivity.java
@@ -1,7 +1,22 @@
 package com.example.andrewpirondini.improvedloginsample;
-
+import android.text.format.Time;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayActivity extends Activity {
@@ -18,4 +33,16 @@ public class DisplayActivity extends Activity {
 
         usernameDisplay.setText(loginBundle.getString("username"));
     }
+
+    private void showTimePickerDialog(View v){
+        DialogFragment newFragment = new OfficeHourFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+
+    }
+
+    private void showDatePickerDialog(View v){
+        DialogFragment newFragment = new OHDatePickerDialog();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
 }
